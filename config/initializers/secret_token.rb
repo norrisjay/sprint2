3d9b7481f87d35cf005d141bddfbdf4d0a8d2a0c
Norris Jay Perez <jay@apptivate.co>
2013-03-10 04:15:08 +0800

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprint2::Application.config.secret_token = '985838d1c45f3dfc80aac3a88d39c650c82adf83e6fa3765ec64df8cc3ba07221df972c635fbe3e4f20e2537728393d8d85950030e5f54164e637876dbf11d8a'
